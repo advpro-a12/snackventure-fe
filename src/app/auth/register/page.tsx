0922c33c751f4206ab73a074cf78b2ed93af1341
Fahmi Ramadhan <51454564+fahmi-ramadhan@users.noreply.github.com>
2024-05-25 13:22:22 +0700

    fix: eslint error and improve style

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from "react";
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 const RegisterPage = () => {
 	const [username, setUsername] = useState("");
@@ -43,10 +44,10 @@ const RegisterPage = () => {
 
 	return (
 		<div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-			<div className="max-w-md w-full space-y-8">
+			<div className="max-w-md w-full space-y-8 bg-white p-8 rounded-lg shadow-md">
 				<div>
-					<h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-						Create your account
+					<h2 className="text-center text-3xl font-semibold text-gray-900">
+						Register to Start
 					</h2>
 				</div>
 				<form className="mt-8 space-y-6" onSubmit={handleRegister}>
@@ -61,7 +62,7 @@ const RegisterPage = () => {
 								name="username"
 								type="text"
 								required
-								className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+								className="appearance-none rounded-t-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:border-2 focus:ring-main focus:border-main focus:z-10 sm:text-sm"
 								placeholder="Username"
 								value={username}
 								onChange={(e) => setUsername(e.target.value)}
@@ -76,7 +77,7 @@ const RegisterPage = () => {
 								name="email"
 								type="email"
 								required
-								className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+								className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:border-2 focus:ring-main focus:border-main focus:z-10 sm:text-sm"
 								placeholder="Email"
 								value={email}
 								onChange={(e) => setEmail(e.target.value)}
@@ -91,7 +92,7 @@ const RegisterPage = () => {
 								name="password"
 								type="password"
 								required
-								className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+								className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:border-2 focus:ring-main focus:border-main focus:z-10 sm:text-sm"
 								placeholder="Password"
 								value={password}
 								onChange={(e) => setPassword(e.target.value)}
@@ -106,7 +107,7 @@ const RegisterPage = () => {
 								name="phoneNumber"
 								type="tel"
 								required
-								className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+								className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:border-2 focus:ring-main focus:border-main focus:z-10 sm:text-sm"
 								placeholder="Phone Number"
 								value={phoneNumber}
 								onChange={(e) => setPhoneNumber(e.target.value)}
@@ -121,30 +122,23 @@ const RegisterPage = () => {
 								name="address"
 								type="text"
 								required
-								className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+								className="appearance-none rounded-b-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:border-2 focus:ring-main focus:border-main focus:z-10 sm:text-sm"
 								placeholder="Address"
 								value={address}
 								onChange={(e) => setAddress(e.target.value)}
 							/>
 						</div>
 					</div>
-
 					<div>
-						<button
-							type="submit"
-							className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-						>
+						<Button type="submit" className="w-full">
 							Register
-						</button>
+						</Button>
 					</div>
 				</form>
 				<div>
 					<p className="mt-2 text-center text-sm text-gray-600">
 						Already have an account?{" "}
-						<Link
-							href="/auth/login"
-							className="font-medium text-indigo-600 hover:text-indigo-500"
-						>
+						<Link href="/auth/login" className="font-medium text-main">
 							Sign In
 						</Link>
 					</p>
