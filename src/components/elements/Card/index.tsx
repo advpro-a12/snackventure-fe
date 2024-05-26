69005301729ab13240213ca5563962488dc10676
Fahmi Ramadhan <51454564+fahmi-ramadhan@users.noreply.github.com>
2024-05-26 18:58:18 +0700

    feat: implement My Snack Box page

--- src/components/elements/Card/index.tsx
+++ src/components/elements/Card/index.tsx
@@ -9,13 +9,13 @@ const Card: React.FC<CardProps> = ({
 	children,
 }) => {
 	return (
-		<div className="bg-black bg-opacity-50 text-white p-4 w-full flex justify-between items-center rounded-md">
+		<div className="bg-black bg-opacity-50 text-white p-4 w-full flex justify-between items-center rounded-md gap-4">
 			<div className="flex justify-start gap-4 items-center">
 				{imageUrl && (
-					<Image src={imageUrl} alt={title} width={300} height={200} />
+					<Image src={imageUrl} alt={title} width={80} height={80} />
 				)}
-				<div className="flex flex-col items-start justify-between">
-					<h1 className="font-semibold">{title}</h1>
+				<div className="flex flex-col items-start justify-between gap-2">
+					<h1 className="font-semibold text-lg">{title}</h1>
 					{description && <p>{description}</p>}
 				</div>
 			</div>
